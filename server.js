0e772ce9509d795fb1c5543e2868a05bfb8a9487
vicentemorales <vmorales121@gmail.com>
2023-04-23 13:10:32 -0700

    added the views forder

--- server.js
+++ server.js
@@ -7,12 +7,24 @@ const PORT = process.env.PORT
 const app = express()
 console.log(PORT)
 
+// MIDDLEWARE (should be after express but fore route)
+app.set('views', __dirname + '/views')
+app.set('view engine', 'jsx')
+app.engine('jsx', require('express-react-views').createEngine())
+
+
 // ROUTES
 app.get('/', (req, res) => {
-  res.send('Welcome to an Awesome App about Breads!')
+    res.send('Welcome to an Awesome App about Breads')
 })
+  
+// Breads ROUTES
+const breadsController = require('./controllers/breads_controller.js')
+app.use('/breads', breadsController)
+  
 
 // LISTEN
 app.listen(PORT, () => {
   console.log('listening on port', PORT);
 })
+
